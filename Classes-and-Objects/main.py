e450f6221b6302edb7cc7ab22774813d3392c90f
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-05-28 23:25:55 +0100

    First Commit

--- Classes-and-Objects/main.py
+++ Classes-and-Objects/main.py
@@ -1,7 +1,28 @@
+from typing import List
+
+
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
+    def __init__(self, name: str, age: int, tracks: List, score: float):
+        self.name = name
+        self.age = int(age)
+        self.tracks = tracks
+        self.score = float(score)
+    
+    def change_name(self, name: str):
+        self.name = name
+
+    def change_age(self, age: int):
+        self.age = int(age)
+    
+    def add_track(self, track):
+        "Adds new track to the `Student` track"
+        self.tracks.append(track)
+
+    def get_score(self):
+        "Returns the students current score."
+        return self.score
+
 
 
 
@@ -11,4 +32,3 @@ Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
-Bob.get_score()
