e9566664c8b741804d8a230a8245a12eee1bc553
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-05-31 22:20:19 +0100

    Initial Commit

--- Rock-Paper-Scissors/rock_paper_scissors.py
+++ Rock-Paper-Scissors/rock_paper_scissors.py
@@ -8,14 +8,15 @@ GAME_RESULTS = {    # this defines what happens by combining the player and comp
     'SP': "cuts",
 }
 
-
 h = "#################################################################"     # just for design
 print(f"""{h}
 {'Welcome to ROCK-PAPER-SCISSORS'.center(len(h))}
 {h}""")
+
 while True:     # starting the game loop
     player = input("\n\tR - ROCK\n\tP - PAPER\n\tS - SCISSORS\nMake your play: ").upper()   # the player makes his/her play
     computer = random.choice(list(GAME_OPTIONS.keys()))     # the computer makes its play by randomly choosing one of the game options
+
     if not GAME_OPTIONS.get(player):
         print("[-] Invalid Option. Please Try Again!")
         continue
@@ -26,10 +27,12 @@ while True:     # starting the game loop
         continue
     else:
         determinant = player + computer     # this is the determinant that the player won by forming the winning keys of the player in `GAME_RESULTS`
+
         if GAME_RESULTS.get(determinant):   # if the determinant exists in the key i.e the player won
             print("\n\tYou WON!!!")
         else:   # if the deterrminant does not exist i.e the computer won
             determinant = determinant[::-1]     # reverses the determinant to make a match
             print("\n\tYou LOSE!!!")
+
         print(f"\n\t{GAME_OPTIONS.get(determinant[0])} {GAME_RESULTS.get(determinant)} {GAME_OPTIONS.get(determinant[1])}")     # prints the right message based on the determinant.
         break
