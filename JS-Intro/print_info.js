b120a76d8839c977decdca39fa3e174c8e2c35a1
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-06-24 08:42:31 +0100

    Initial Commit

---
+++ JS-Intro/print_info.js
@@ -0,0 +1,6 @@
+const NAME = "Farouk Abiodun, ADEGBOYEGA";
+const HEIGHT = "5'10\"";
+const COUNTRY = "Nigeria";
+
+// Printing the informations to the browser.
+alert("Hello, my name is " + NAME + "\nI am " + HEIGHT + " tall and I'm from " + COUNTRY)
